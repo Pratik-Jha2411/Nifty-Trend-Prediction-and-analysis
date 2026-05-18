a8821d12093301cf8954143f1f9beecbd8c785cb
Pratik Jha <pjha94466@gmail.com>
2026-05-18 13:52:58 +0530

    Fix GitHub notebook rendering issue (remove widget metadata)

---
+++ fix_notebooks.py
@@ -0,0 +1,10 @@
+﻿import json
+import glob
+for f in glob.glob('*.ipynb'):
+    with open(f, 'r', encoding='utf-8') as file:
+        nb = json.load(file)
+    if 'metadata' in nb and 'widgets' in nb['metadata']:
+        del nb['metadata']['widgets']
+        with open(f, 'w', encoding='utf-8') as file:
+            json.dump(nb, file, indent=1)
+        print(f'Fixed {f}')
